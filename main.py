51c7e21b0fcddf2fb62e06037176826002ffac15
Anaru03 <dele22428@uvg.edu.gt>
2023-02-25 08:37:05 -0600

    Declaración de variables y parametros

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import simpy
 import random
+import simpy
 
 
 
-print("Hello, world")
+print("Simulación en curso")
 """
 def car(env):
      while True:
@@ -19,8 +19,22 @@ env.process(car(env))
 env.run(until=50)
 """
 
-def proceso(env, cantRam, cantInstrucciones):
-    global tiempo
+init = 100 
+capacity = 100 #ram memory capacity (100 y 200)
+time_ini = 0
+rango = 10 #Intervalos (1 y 5)
+inst = 3 #instrucciones del ciclo (6)
+operacion = 1 #operaciones por ciclo
+procesos_cant = 25  #number of processes (25,50,100,150 y 200)
+
+def proceso(env, cantRam, cantInstrucciones, id_proceso, inst, operacion, memoria_disponible, acceso_procesador, tiempo_inicio):
+    inicial_proced =env.now 
+   
+    
+    
+    
+    
+    
 
 
 
